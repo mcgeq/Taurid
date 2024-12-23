decab473afdaaba8efa96fe0e319ec768c35de3d
mcge <mcgeq@outlook.com>
2024-12-23 19:46:48 +0800

    refactor(back): :art: Refactor lib

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,7 @@
 // File:           lib.rs
 // Description:    Application start main
 // Create   Date:  2024-12-22 15:23:02
-// Last Modified:  2024-12-22 17:40:09
+// Last Modified:  2024-12-23 19:42:13
 // Modified   By:  mcge <mcgeq@outlook.com>
 // ----------------------------------------------------------------------------
 mod common;
@@ -39,57 +39,39 @@ pub fn run() {
     let _guard = setup();
     info!("All configurations have benn loaded.");
     info!("Application starting...");
-
+    let mut builder = tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_upload::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_fs::init());
+    
     #[cfg(desktop)]
     {
-        setup_desktop();
+        builder = builder
+            .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+                if let Some(w) = app.get_webview_window("main") {
+                    info!("Application instance already running, focusing existing window.");
+                    w.set_focus().unwrap();
+                }
+            }))
+            .plugin(tauri_plugin_updater::Builder::new().build())
+            .plugin(tauri_plugin_window_state::Builder::new().build())
+            .plugin(tauri_plugin_process::init());
     }
+    
     #[cfg(mobile)]
     {
-        setup_mobile();
-    }
-}
-
-#[cfg(desktop)]
-fn setup_desktop() {
-    info!("Application from Desktop!!!");
-    tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::new().build())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_upload::init())
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
-            if let Some(w) = app.get_webview_window("main") {
-                info!("Application instance already running, focusing existing window.");
-                w.set_focus().unwrap();
-            }
-        }))
-        .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![show_main_window, logmg, greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
-
-#[cfg(mobile)]
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-fn setup_mobile() {
-    use tauri::window;
-
-    info!("Application from Mobile!!!");
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_os::init())
-        .setup(|app| {
+        #[cfg_attr(mobile, tauri::mobile_entry_point)]
+        builder = builder.setup(|app| {
             window::setup_mobile_window(app.handle())?;
             Ok(())
-        })
+        });
+    }
+    
+    builder
         .invoke_handler(tauri::generate_handler![show_main_window, logmg, greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
